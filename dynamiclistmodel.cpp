3a8ac070d82c224c3bd129003c038211d049916d
Gregor Fabritius <fabritius@grefab.de>
2014-08-05 02:50:38 +0200

    First try of implementation.

--- dynamiclistmodel.cpp
+++ dynamiclistmodel.cpp
@@ -1,5 +1,7 @@
 #include "dynamiclistmodel.h"
 
+#include <QDebug>
+
 DynamicListModel::DynamicListModel(QObject* parent) :
     QAbstractListModel(parent)
 {
@@ -19,10 +21,18 @@ void DynamicListModel::dequeueStuff()
 
 int DynamicListModel::rowCount(const QModelIndex& parent) const
 {
+    Q_UNUSED(parent)
+
     return stuff_.count();
 }
 
 QVariant DynamicListModel::data(const QModelIndex& index, int role) const
 {
+    qDebug() << role;
 
+    if( index.row() >= 0 && index.row() < stuff_.size() ) {
+        return QVariant(stuff_[index.row()]);
+    }
+
+    return QVariant();
 }
